122dddd2d4024a2c7ab9bc1f7ffb1b70a2c857c9
Cassondra Foesch <puellanivis@gmail.com>
2017-11-29 20:30:14 +0000

    update nrj.sh script to handle OSKIND/ARCH that is expected generation from masher

--- nrj.sh
+++ nrj.sh
@@ -1,3 +1,26 @@
-#/bin/sh
+#/bin/bash
 
-exec ./icycat --logtostderr --stderrthreshold=INFO http://cdn.nrjaudio.fm/adwz1/de/33001/mp3_128.mp3 | mpv /dev/stdin
+# canonicalize OSTYPE to OSKIND
+case $OSTYPE in
+darwin*)        OSKIND=darwin ;;
+linux*)         OSKIND=linux ;;
+openbsd*)       OSKIND=openbsd ;;
+freebsd*)       OSKIND=freebsd ;;
+*)              OSKIND=$OSTYPE ;;
+esac
+
+# canonicalize CPU to ARCH
+[ -z "$CPU" ] && CPU=`uname -m`
+case $CPU in
+amd64|x86_64|x86-64)    ARCH=x86_64 ;;
+i386|x86)               ARCH=x86 ;;
+*)                      ARCH=$CPU
+esac
+
+which mpv > /dev/null 2>&1
+if [[ $? -ne 0 ]]; then	
+	echo "This script requires mpv to play the stream." >&2
+	exit 1
+fi
+
+exec ./bin/$OSKIND.$ARCH/icycat --logtostderr --stderrthreshold=INFO http://cdn.nrjaudio.fm/adwz1/de/33001/mp3_128.mp3 | mpv /dev/stdin
